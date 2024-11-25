b2c1af0b0dcee666d47a98aaaaa9d2a6edb92b40
reavanth <reavanthdharani@gmail.com>
2024-11-25 11:24:07 +0530

    Axios using api handling finish!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import './App.css'
+import React, { useEffect, useState } from "react";
 
 function App() {
+  let [id, setId] = useState("");
+  const [response, setResponse] = useState({});
+
+  useEffect(() => {
+    if (id) {
+      axios
+        .get(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          setResponse(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [id]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>
+        <div className="row header">
+          <h2>Getting Random user's data using API</h2>
+        </div>
+        <div >
+          <div >
+            <label>Search Users via Id:</label>
+            <br />
+            <input
+              type="number"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+              placeholder="Enter User ID"
+            />
+            <br />
+          </div>
+          <div >
+           <p> ID:   {response.id}</p>
+          <p>Name:   {response.name}</p>
+          <p>Username:   {response.username}</p>
+            <p>Email:   
+            {response.email}</p>
+           
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
